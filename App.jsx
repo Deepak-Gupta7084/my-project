2eed1fe4d199638400fdac2e0a39ecd276a14bf1
Deepak-Gupta7084 <dkp1827@gmail.com>
2025-06-20 11:10:04 -0700

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,52 +1,25 @@
 import React, { useState } from 'react';
-import Todo from './Todo';
+import PostForm from './PostForm';
+import PostList from './PostList';
 
 function App(){
-  const [todos, setTodos] = useState([]);
-  const [task, setTask] = useState("");
+  const [posts, setPosts] = useState([]);
 
-  const addTodo=()=>{
-    if(task.trim() === "") return;
-    const newTodo ={
-      id: Date.now(),
-      text: task,
-      completed: false,
-    };
-    setTodos([...todos, newTodo]);
-    setTask("");
+  const addPost = (post) => {
+    setPosts([post, ...posts]);
   };
 
-  const toggleComplete=(id) => {
-    setTodos(
-      todos.map(todo => 
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
+  const deletePost = (id) => {
+    setPosts(posts.filter((posts) => post.id !== id));
   };
 
   return(
     <div className="container">
-      <h1>React To-Do App</h1>
-      <div className="input-container">
-        <input type="text" placeholder="Add a task"
-        value={task}
-        onChange={(e) => setTask(e.target.value)} />
-        
-        <button onClick={addTodo}>Add</button>
-      </div>
-
-      <ul>
-        {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo}
-          toggleComplete={toggleComplete}
-          deleteTodo={deleteTodo} />
-        ))}
-      </ul>
+      <h1>React Blog App</h1>
+      <PostForm onAdd={addPost} />
+      <PostList posts={posts} onDelete={deletePost} />
     </div>
   );
 }
+
 export default App;
